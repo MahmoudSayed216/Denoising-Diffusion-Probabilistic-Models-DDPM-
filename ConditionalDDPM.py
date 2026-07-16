52629f8fa1902e85b20cc4bc41daa80519547f4e
MahmoudSayed216 <mahmoud_das@outlook.com>
2026-07-17 02:15:30 +0300

    added positional embedding

--- ConditionalDDPM.py
+++ ConditionalDDPM.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch
-
+from math import log
 
 
 
@@ -20,15 +20,21 @@ class Embedder(nn.Module):
 
 
 class SinudoidalPositionalEmbedding(nn.Module):
-    def __init__(self, embedding_dim, n_timestamps):
+    def __init__(self, embedding_dim):
         super().__init__()
-        numbers = torch.arange(start=0, end=n_timestamps)
-        
+        self.embedding_dim = embedding_dim
+        self.ln10000 = log(10000)
+    
+    def forward(self, timestep):
+        vec = torch.arange(start=0, end=int(self.embedding_dim/2))
+        vec = vec * self.ln10000/(self.embedding_dim/2 - 1)
+        vec = vec*timestep
+
 
 
 
 class ConditionalDDPM(nn.Module):
-    def __init__(self, n_timestamps, num_classes, image_side_length, embedding_dim):
+    def __init__(self, n_timesteps, num_classes, image_side_length, embedding_dim):
         super().__init__()
 
         self.class_embedder = Embedder(num_classes, embedding_dim, image_side_length)
--- tests.py
+++ tests.py
@@ -1,14 +1,38 @@
+# import torch
+
+
+
+# tensor = torch.tensor([1,2,3,4])
+# print(tensor)
+
+# tensor = tensor.view((2, 2))
+# print(tensor)
+
+
+# numbers = torch.arange(start=0, end=10)
+# print(numbers)
+
+
 import torch
+import math
+
+
+def forward(timestep):
+    embedding_dim = 4
+    ln10000 = math.log(10000)
+    vec = torch.exp(-torch.arange(embedding_dim/2)*ln10000/(embedding_dim/2 - 1))
+    vec = timestep*vec
+    
+    sin = torch.sin(vec)
+    cos = torch.cos(vec)
+    
+    emb = torch.cat([sin, cos])
+
+    return emb
 
 
 
-tensor = torch.tensor([1,2,3,4])
-print(tensor)
-
-tensor = tensor.view((2, 2))
-print(tensor)
+print(forward(1))
 
 
-numbers = torch.arange(start=0, end=10)
-print(numbers)
-
+    
